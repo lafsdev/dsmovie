919b1b46acbb35262756aa81fcbfa0276fa851ce
Leandro Alves de Fontes Silva <lafs.dev@gmail.com>
2022-01-10 23:06:33 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Form() {
+    return (
+        <div>
+            <h1>Página do Formulário</h1>
+        </div>
+    )
+}
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Listing() {
+    return (
+        <div>
+            <h1>Página de Listagem</h1>
+        </div>
+    )
+}
